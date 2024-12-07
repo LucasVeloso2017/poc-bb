0ec9e3dd12dbbf93109f34976700834faf7c4486
Lveloso <lucas.veloso@mail.com>
2024-12-07 16:49:40 -0300

    fix: fix header design

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,39 @@
-import { Container, LogoWrapper, ProfileContainer, ProfileText, TabsWrapper, TextHeader } from "./styles";
+import { Container, LogoWrapper, ProfileContainer, ProfileText, TabsWrapper } from "./styles";
 
 import CalendarLogo from '../../assets/Calendar.png';
 import ProfileImg from '../../assets/Profile.png';
 import GoDown from '../../assets/chevron-down.png';
+import { useMemo } from "react";
 
 export function Header() {
+  const isIntegrationsActiveTab = useMemo(() => window.location.pathname.includes('integrations'),[])
+  const isIdentityActiveTab = useMemo(() => window.location.pathname.includes('identity'),[])
+  const isDataActiveTab = useMemo(() => window.location.pathname.includes('data'),[])
+  const isAgentActiveTab = useMemo(() => window.location.pathname.includes('agent'),[])
+
+  
   return(
     <Container>
       <LogoWrapper>
         <img src={CalendarLogo} alt="logo de um calendario" />
-        <TextHeader>Hub de Integrações</TextHeader>
+        <h1>Hub de Integrações</h1>
       </LogoWrapper>
       <TabsWrapper>
-        <a href="#">Integração</a>
-        <a href="#">Identidade</a>
-        <a href="#">Dados</a>
-        <a href="#">Agente</a>
+        <div className={`item ${isIntegrationsActiveTab && 'active'}`}>
+          <a href="#">Integração</a>
+        </div>
+
+        <div className={`item ${isIdentityActiveTab && 'active'}`}>
+          <a href="#">Identidade</a>
+        </div>
+
+        <div className={`item ${isDataActiveTab && 'active'}`}>
+          <a href="#">Dados</a>
+        </div>
+
+        <div className={`item ${isAgentActiveTab && 'active'}`}>
+          <a href="#">Agente</a>
+        </div>
       </TabsWrapper>
       <ProfileContainer>
         <img src={ProfileImg} alt="Foto do usuário" />
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,63 +1,85 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
+  width: 100%;
   display: flex;
   flex-direction: row;
   align-items: center;
-  width: 100%;
   height: 6.6rem;
   background-color: #fff;
   box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.15);
-
-  img {
-    margin: 2.2rem 0.3rem 2.4rem 3.4rem;
-  }
-`;
-
-export const TextHeader = styled.h1`
-  font-family: 'Noto Sans', sans-serif;
-  font-size: 1.8rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 2.2rem;
-  color: #003E43;
 `;
 
 export const LogoWrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  margin-left: 3.4rem;
   gap: 1.5rem;
+
+  h1 {
+    font-family: "Noto Sans", sans-serif;
+    font-size: 1.8rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 2.2rem;
+    color: #003e43;
+  }
 `;
 
 export const TabsWrapper = styled.div`
+  height: 100%;
   display: flex;
-  flex-direction: row;
   align-items: center;
+  justify-content: center;
   gap: 1.6rem;
 
   margin-left: 3.5rem;
 
-  a {
-    font-family: 'Noto Sans', sans-serif;
-    font-size: 1.6rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 2.4rem;
-    text-decoration: none;
-    color: #003E43;
+  .item {
+    height: 100%;
+    padding: 0.8rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    a {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.6rem;
+      font-style: normal;
+      font-weight: 400;
+      text-decoration: none;
+      color: #003e43;
+
+      position: relative;
+    }
+  }
+
+  .active {
+    a::before {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      top: 40px;
+      width: 100%;
+      height: 0.4rem;
+      border-top-right-radius: 0.4rem;
+      border-top-left-radius: 0.4rem;
+      background-color: #00aeba;
+    }
   }
 `;
 
 export const ProfileContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  flex:1 1 auto;
   align-items: center;
-  gap: 2.0rem;
-  margin-left: 46.0rem;
+  justify-content: flex-end;
+  gap: 2rem;
+  margin-right: 4rem;
 
   p {
-    font-family: 'Noto Sans', sans-serif;
+    font-family: "Noto Sans", sans-serif;
     font-size: 1.4rem;
     font-style: normal;
     font-weight: 600;
@@ -66,7 +88,7 @@ export const ProfileContainer = styled.div`
   }
 
   span {
-    font-family: 'Noto Sans', sans-serif;
+    font-family: "Noto Sans", sans-serif;
     font-size: 1.4rem;
     font-style: normal;
     font-weight: 600;
