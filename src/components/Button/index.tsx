611835b0423c514717463881fa8db8fc6a8f62ee
Lveloso <lucas.veloso@mail.com>
2024-12-07 17:41:29 -0300

    feat: adjusts on button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,43 +1,49 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { ButtonHTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes, CSSProperties } from "react";
 
-import * as S from './styles';
+import * as S from "./styles";
 
 type ButtonProps = {
-   variant?:'brand'
-   label?:string
-   withIcon?:{
-      icon:any,
-      position?: 'left' | 'right'
-      onlyIcon?:boolean
-   }
-} & ButtonHTMLAttributes<HTMLButtonElement>
+  variant?: "brand" | "brand-outlined";
+  label?: string;
+  withIcon?: {
+    icon: any;
+    position?: "left" | "right";
+    onlyIcon?: boolean;
+    styles?: CSSProperties;
+  };
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({variant,label,withIcon,...buttonProps}:ButtonProps) => {
-
-   if(withIcon?.onlyIcon){
-      return(
-         <S.Container {...buttonProps} isBrandVariant={variant === 'brand'}>
-            <img src={withIcon.icon} alt="input-icon" />
-         </S.Container>
-      )
-   }
-
-  return(
-      <S.Container {...buttonProps} isBrandVariant={variant === 'brand'}>
-         {withIcon && withIcon.position === 'left' && (
-            <img src={withIcon.icon} alt="input-icon" />
-         )}
-
-         {label && (
-            <span>{label}</span>
-         )}
-
-         {withIcon && withIcon.position === 'right' && (
-            <img src={withIcon.icon} alt="input-icon" />
-         )}
+const Button = ({ variant, label, withIcon, ...buttonProps }: ButtonProps) => {
+  if (withIcon?.onlyIcon) {
+    return (
+      <S.Container
+        {...buttonProps}
+        isBrandVariant={variant === "brand"}
+        isBrandOutlinedVariant={variant === "brand-outlined"}
+      >
+        <img src={withIcon.icon} alt="input-icon" style={withIcon.styles} />
       </S.Container>
+    );
+  }
+
+  return (
+    <S.Container
+      {...buttonProps}
+      isBrandVariant={variant === "brand"}
+      isBrandOutlinedVariant={variant === "brand-outlined"}
+    >
+      {withIcon && withIcon.position === "left" && (
+        <img src={withIcon.icon} alt="input-icon" style={withIcon.styles} />
+      )}
+
+      {label && <span>{label}</span>}
+
+      {withIcon && withIcon.position === "right" && (
+        <img src={withIcon.icon} alt="input-icon" style={withIcon.styles} />
+      )}
+    </S.Container>
   );
-}
+};
 
 export default Button;
