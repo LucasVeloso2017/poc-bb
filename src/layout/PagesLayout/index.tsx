7d65f8c1c6c449f93ec74bcc152590a16e79acd3
Lveloso <lucas.veloso@mail.com>
2024-12-07 16:49:53 -0300

    feat: apply header on layout

--- src/layout/PagesLayout/index.tsx
+++ src/layout/PagesLayout/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import * as S from './styles';
 import { Outlet } from 'react-router';
+import { Header } from '../../components/Header';
 
 const PagesLayout = () => {
   return(
-   <S.Container>
-      <Outlet/>
-   </S.Container>
+    <>
+      <Header/>
+      <S.Container>
+        <Outlet/>
+      </S.Container>
+   </>
+
   );
 }
 
