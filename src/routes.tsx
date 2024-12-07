49f03647d53d066c96ff5f93e9909454a3780930
Lveloso <lucas.veloso@mail.com>
2024-12-07 15:50:20 -0300

    feat: publish page and fix them

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import PagesLayout from "./layout/PagesLayout";
 import Main from "./pages/Main";
 import Fullscreen from "./pages/Fullscreen";
 import LoginLayout from "./layout/LoginLayout";
+import AllIntegrations from "./pages/Integrations/All";
 // import { Container } from './styles';
 
 const AppRoutes = () => {
@@ -11,16 +12,19 @@ const AppRoutes = () => {
    <BrowserRouter>
     <Routes>
       <Route element={<LoginLayout/>}>
-        <Route path="/" element={<SignIn />} />
+        <Route path="/" index element={<SignIn />} />
       </Route>
 
       <Route element={<PagesLayout />}>
         <Route path="/main" element={<Main />} />
         <Route path="/fullscreen" element={<Fullscreen />} />
       </Route>
+
+      <Route path="/integrations" element={<PagesLayout />}>
+        <Route path="/integrations/all" element={<AllIntegrations />} />
+      </Route>
+
     </Routes>
-
-
    </BrowserRouter>
   );
 }
