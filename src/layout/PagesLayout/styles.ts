d54d35fc71362f2f82c68c3f497c730a57994466
Lveloso <lucas.veloso@mail.com>
2024-12-07 15:50:20 -0300

    feat: publish page and fix them

--- src/layout/PagesLayout/styles.ts
+++ src/layout/PagesLayout/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
    width: 100%;
-   height: 100vh;
    background-color: #F6FAFA;
    padding-left: 10.8rem;
    padding-right: 10.8rem;
