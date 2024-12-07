8d9622d3902ad700c2dd5ed40bbb242083bab62f
Brenobn <brenobragabn@gmail.com>
2024-12-07 11:21:17 -0300

    Importando e adicionando GlobalStyles no arquivo main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppRoutes from './routes.tsx'
+import { GlobalStyles } from './styles/global.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyles />
     <AppRoutes/>
   </StrictMode>,
 )
