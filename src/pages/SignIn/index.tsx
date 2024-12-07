beabb9544ee5818d9427b0ab59c01f26f41fe1fb
Brenobn <brenobragabn@gmail.com>
2024-12-07 00:26:48 -0300

    Importando o Footer e modificando alguns estilos

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,6 @@
-import { Container, Background } from "./styled";
+import { Container, Background, FormContainer, Title } from "./styled";
+
+import { Footer } from "../../components/Footer";
 
 import Logo from "../../assets/image.png";
 
@@ -7,7 +9,30 @@ export function SignIn() {
     <Container>
       <Background>
         <img src={Logo} alt="Logo do app" />
+
+        <FormContainer>
+          <div>
+            <Title>Faça seu login</Title>
+
+            <label>
+              E-mail/CPF
+              <input type="text" />
+            </label>
+
+            <label>
+              Senha
+              <input type="password" />
+            </label>
+
+            <div className="buttons">
+              <button className="first">Acessar</button>
+              <button className="second">Entrar com Microsoft</button>
+            </div>
+            <a href="#">Esqueci minha senha</a>
+          </div>
+        </FormContainer>
       </Background>
+      <Footer />
     </Container>
   );
 }
--- src/pages/SignIn/styled.ts
+++ src/pages/SignIn/styled.ts
@@ -9,16 +9,106 @@ export const Container = styled.div`
 
 export const Background = styled.div`
   width: 100%; 
-  height: 100vh;
+  flex: 1;
   background: url(${LoginBackground}) no-repeat center;
   background-size: cover;
-  position: absolute;
 
   > img {
     width: 38%;
     height: auto;
-    position: relative;
 
     padding: 6.0rem 11.6rem 0 9.0rem;
   }
+`;
+
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  > div {
+    width: 45.0rem;
+    height: 45.0rem;
+    flex-shrink: 0;
+
+    background-color: white;
+    border-radius: 2rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2.2rem;
+  }
+
+  label {
+    font-family: 'Inter', sans-serif;
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 400;
+    color: gray;
+  }
+
+  input {
+    display: flex;
+    height: 3.6rem;
+    width: 40rem;
+    margin-top: 1rem;
+    border: 1px solid #cecece;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    padding: 0.5rem;
+    outline: none;
+    transition: all 0.3s ease-in-out;
+    border-radius: 0.6rem;
+  }
+
+  .buttons {
+    display: flex;
+    flex-direction: column;
+    gap: 0.3rem;
+  }
+
+  .first {
+    width: 40rem;
+    height: 5.4rem;
+    background-color: turquoise;
+    border-radius: 0.5rem;
+    
+    color: white;
+    font-family: 'Inter', sans-serif;
+    font-size: 1.8rem;
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  .second {
+    width: 40rem;
+    height: 5.4rem;
+    background-color: white;
+    border-radius: 0.5rem;
+    
+    color: black;
+    font-family: 'Inter', sans-serif;
+    font-size: 1.8rem;
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  a {
+    font-family: 'Inter', sans-serif;
+    font-size: 1.2rem;
+    font-weight: 400;
+    text-decoration: none;
+    color: black;
+  }
+`;
+
+export const Title = styled.h3`
+  font-family: 'Inter', sans-serif;
+  font-size: 2.0rem;
+  font-weight: 700;
+  font-style: normal;
+  color: black;
+
+  margin: 3rem 26rem 2rem 0;
 `;
