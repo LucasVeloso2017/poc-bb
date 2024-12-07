6afbf170ae3dee2b40896f2f560e6e8c8b60e33e
Lveloso <lucas.veloso@mail.com>
2024-12-07 12:33:19 -0300

    fix: fix routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import { SignIn }  from "./pages/SignIn";
+import PagesLayout from "./layout/PagesLayout";
+import Main from "./pages/Main";
+import Fullscreen from "./pages/Fullscreen";
+import LoginLayout from "./layout/LoginLayout";
 // import { Container } from './styles';
 
 const AppRoutes = () => {
   return(
    <BrowserRouter>
     <Routes>
-      <Route path="/" element={<SignIn />} />
+      <Route element={<LoginLayout/>}>
+        <Route path="/" element={<SignIn />} />
+      </Route>
+
+    <Route element={<PagesLayout />}>
+      <Route path="/main" element={<Main />} />
+      <Route path="/fullscreen" element={<Fullscreen />} />
+    </Route>
     </Routes>
+
+
    </BrowserRouter>
   );
 }
