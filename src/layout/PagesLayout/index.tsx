47d3c2e58f246c9aa6a9816409f93c571f87c656
Lveloso <lucas.veloso@mail.com>
2024-12-08 20:45:48 -0300

    fix: fix layout to scroll ever to top

--- src/layout/PagesLayout/index.tsx
+++ src/layout/PagesLayout/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import * as S from './styles';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import { Header } from '../../components/Header';
 
 const PagesLayout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); 
+  }, [pathname]);
+
   return(
     <>
       <Header/>
