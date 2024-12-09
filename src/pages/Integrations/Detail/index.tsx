e9ab55c5612245833c241ffa9ae081ab8d16c505
Lveloso <lucas.veloso@mail.com>
2024-12-08 22:40:21 -0300

    feat: create Integration detail page

--- src/pages/Integrations/Detail/index.tsx
+++ src/pages/Integrations/Detail/index.tsx
@@ -1,61 +1,259 @@
-import { BoxTable, BoxWrapper, Container, Display, GraphicDisplay, TextLink, TimeGraphic } from "./styles";
+import * as S from "./styles";
 
-import GoLeft from "../../../assets/chevron-left.png";
-import Arrows from "../../../assets/arrows.svg";
-import Clock from "../../../assets/clock.svg";
+import ChevronLeft from "../../../assets/chevron-left.png";
+import ArrowsClosedIcon from "../../../assets/arrows-closed-icon.svg";
+import ClockIcon from "../../../assets/clock-icon.svg";
+import CheckCircleIcon from "../../../assets/check-circle-icon.svg";
+import Table from "../../../components/Table";
 
-function IntegrationDetail() {
-  return(
-    <Container>
-      <TextLink>
-        <img src={GoLeft} alt="Seta para a esquerda" />
-        <h1>Integração Plurall</h1>
-      </TextLink>
-      <Display>
-        <div className="itens-wrapper">
-          <span>Tipo</span>
-          <p>API - DB</p>
-        </div>
-        <div className="itens-wrapper">
-          <span>Nome das Partes</span>
-          <p>API: plurall BD: sesidn postgresql</p>
-        </div>
-        <div className="itens-wrapper">
-          <span>Último Play</span>
-          <p>20/01 - 10h00</p>
-        </div>
-        <div className="itens-wrapper">
-          <span>Último Stop</span>
-          <p>20/01 - 10h00</p>
-        </div>
-      </Display>
-      <BoxWrapper>
-        <GraphicDisplay>
-          <div className="text-and-icon">
-            <img src={Arrows} alt="Setas em formato de icone" />
-            <h1>Últimas integrações</h1>
+function IntegrationsDetail() {
+
+  const edTechColumns = [
+    {
+      headerCell: (
+        <S.EdTechNameTableCollumn>
+          <div className="table-header">
+            <span>Nome</span>
           </div>
-          <p>Integrações diárias</p>
-        </GraphicDisplay>
-        <GraphicDisplay>
-          <div className="text-and-icon">
-            <img src={Arrows} alt="setas em formato de icone" />
-            <h1>Produtos</h1>
+        </S.EdTechNameTableCollumn>
+      ),
+      rowCell: () => (
+        <S.EdTechNameTableCollumn>
+          <div className="table-row">
+            <span>Tarefas</span>
           </div>
-          <p>Status da EdTech</p>
-        </GraphicDisplay>
-      </BoxWrapper>
-      <TimeGraphic>
-        <div className="text-and-icon">
-          <img src={Clock} alt="Icone de um relogio" />
-          <h1>Tempo médio de resposta</h1>
-        </div>
-      </TimeGraphic>
-      <BoxTable>
-        
-      </BoxTable>
-    </Container>
+        </S.EdTechNameTableCollumn>
+      ),
+    },
+    {
+      headerCell: (
+        <S.EdTechStatusTableCollumn>
+          <div className="table-header">
+            <span>Status</span>
+          </div>
+        </S.EdTechStatusTableCollumn>
+      ),
+      rowCell: () => (
+        <S.EdTechStatusTableCollumn>
+          <div className="table-row">
+            <div className="green-dot"></div>
+            <span>Ativado</span>
+          </div>
+        </S.EdTechStatusTableCollumn>
+      ),
+    },
+  ]
+
+  const edTechData = Array.from({ length: 4 })
+
+  const edtechDatalogColumns = [
+    {
+      headerCell: (
+        <S.EdTechDatalogStatusTableCollumn>
+          <div className="table-header">
+            <span>Status</span>
+          </div>
+        </S.EdTechDatalogStatusTableCollumn>
+      ),
+      rowCell: () => (
+        <S.EdTechDatalogStatusTableCollumn>
+          <div className="table-row">
+            <div className="status-ok">
+              <img src={CheckCircleIcon} alt="" />
+            </div>
+          </div>
+        </S.EdTechDatalogStatusTableCollumn>
+      ),
+    },
+    {
+      headerCell: (
+        <S.EdTechDatalogDescriptionTableCollumn>
+          <div className="table-header">
+            <span>Descrição</span>
+          </div>
+        </S.EdTechDatalogDescriptionTableCollumn>
+      ),
+      rowCell: () => (
+        <S.EdTechDatalogDescriptionTableCollumn>
+          <div className="table-row">
+            <span>
+              Tentativa de chamar a API
+            </span>
+          </div>
+        </S.EdTechDatalogDescriptionTableCollumn>
+      ),
+    },
+    {
+      headerCell: (
+        <S.EdTechDatalogDateStartTableCollumn>
+          <div className="table-header">
+            <span>Data início</span>
+          </div>
+        </S.EdTechDatalogDateStartTableCollumn>
+      ),
+      rowCell: () => (
+        <S.EdTechDatalogDateStartTableCollumn>
+          <div className="table-row">
+            <span>20/01/2024 - 10h00</span>
+          </div>
+        </S.EdTechDatalogDateStartTableCollumn>
+      ),
+    },
+    {
+      headerCell: (
+        <S.EdTechDatalogDateEndTableCollumn>
+          <div className="table-header">
+            <span>Data término</span>
+          </div>
+        </S.EdTechDatalogDateEndTableCollumn>
+      ),
+      rowCell: () => (
+        <S.EdTechDatalogDateEndTableCollumn>
+          <div className="table-row">
+            <span>20/01/2024 - 10h00</span>
+          </div>
+        </S.EdTechDatalogDateEndTableCollumn>
+      ),
+    },
+    {
+      headerCell: (
+        <S.EdTechDatalogLatencyTableCollumn>
+          <div className="table-header">
+            <span>Latência</span>
+          </div>
+        </S.EdTechDatalogLatencyTableCollumn>
+      ),
+      rowCell: () => (
+        <S.EdTechDatalogLatencyTableCollumn>
+          <div className="table-row">
+            <span>
+              10s
+            </span>
+          </div>
+        </S.EdTechDatalogLatencyTableCollumn>
+      ),
+    },
+    {
+      headerCell: (
+        <S.EdTechDatalogProcessTableCollumn>
+          <div className="table-header">
+            <span>Quant. Obj. Processados</span>
+          </div>
+        </S.EdTechDatalogProcessTableCollumn>
+      ),
+      rowCell: () => (
+        <S.EdTechDatalogProcessTableCollumn>
+          <div className="table-row">
+            <span>
+              1000
+            </span>
+          </div>
+        </S.EdTechDatalogProcessTableCollumn>
+      ),
+    },
+    {
+      headerCell: (
+        <S.EdTechDatalogErrorTableCollumn>
+          <div className="table-header">
+            <span>Erros</span>
+          </div>
+        </S.EdTechDatalogErrorTableCollumn>
+      ),
+      rowCell: () => (
+        <S.EdTechDatalogErrorTableCollumn>
+          <div className="table-row">
+            <span>
+              5
+            </span>
+          </div>
+        </S.EdTechDatalogErrorTableCollumn>
+      ),
+    },
+  ]
+
+  const edTechDatalogData = Array.from({ length: 2 })
+
+
+  return (
+    <S.Container>
+      <S.HeaderRow>
+        <a href="/integrations">
+          <img src={ChevronLeft} />
+          Integração Plurall
+        </a>
+      </S.HeaderRow>
+      <S.Card>
+        <S.InfoDetails>
+          <div className="itens-wrapper">
+            <span>Tipo</span>
+            <p>API - DB</p>
+          </div>
+          <div className="itens-wrapper">
+            <span>Nome das Partes</span>
+            <p>API: plurall BD: sesidn postgresql</p>
+          </div>
+          <div className="itens-wrapper">
+            <span>Último Play</span>
+            <p>20/01 - 10h00</p>
+          </div>
+          <div className="itens-wrapper">
+            <span>Último Stop</span>
+            <p>20/01 - 10h00</p>
+          </div>
+        </S.InfoDetails>
+      </S.Card>
+      <S.GrafficDataWrapper>
+        <S.Card>
+          <S.IntegrationsWrapper>
+            <div className="header">
+              <div className="title">
+                <img src={ArrowsClosedIcon} alt="" />
+                <h1>Últimas integrações</h1>
+              </div>
+              <span>Integrações diárias</span>
+            </div>
+            <div className="content">
+              Grafico donnut
+            </div>
+          </S.IntegrationsWrapper>
+        </S.Card>
+        <S.Card>
+          <S.IntegrationsWrapper>
+            <div className="header">
+              <div className="title">
+                <img src={ArrowsClosedIcon} alt="" />
+                <h1>Produtos</h1>
+              </div>
+              <span>Status da EdTech</span>
+            </div>
+            <div className="content">
+              <Table columns={edTechColumns} data={edTechData} />
+            </div>
+          </S.IntegrationsWrapper>
+        </S.Card>
+      </S.GrafficDataWrapper>
+      <S.Card>
+        <S.TimestampMetric>
+          <div className="header">
+            <div className="title">
+              <img src={ClockIcon} alt="" />
+              <h1>Tempo médio de resposta</h1>
+            </div>
+          </div>
+          <div className="content">
+            flow-chart
+          </div>
+        </S.TimestampMetric>
+      </S.Card>
+      <br />
+      <S.Card>
+        <S.EdTechDatalog>
+          <Table data={edTechDatalogData} columns={edtechDatalogColumns}/>
+        </S.EdTechDatalog>
+      </S.Card>
+      <br /><br /><br />
+    </S.Container>
   );
 }
 
-export default IntegrationDetail
+export default IntegrationsDetail
--- src/pages/Integrations/Detail/styles.ts
+++ src/pages/Integrations/Detail/styles.ts
@@ -2,35 +2,52 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
-  background-color: #f6fafa;
 `;
 
-export const TextLink = styled.div`
+export const HeaderRow = styled.div`
+  width: 100%;
   display: flex;
-  flex-direction: row;
+  justify-content: flex-start;
   align-items: center;
-  gap: 0.8rem;
-  margin-top: 3.8rem;
+  margin-bottom: 1.7rem;
 
-  h1 {
-    font-family: 'Noto Sans', sans-serif;
-    font-size: 1.6rem;
+  a {
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.8rem;
+    font-family: "Noto Sans", sans-serif;
     font-style: normal;
-    font-weight: 700;
-    line-height: normal;
     color: #354344;
+    font-weight: 700;
+    font-size: 1.6rem;
+  }
+
+  span {
+    font-family: "Noto Sans", sans-serif;
+    font-style: normal;
+    color: #354344;
+    font-weight: 400;
+    font-size: 1.4rem;
   }
 `;
 
-export const Display = styled.div`
+export const Card = styled.div`
+  overflow: hidden;
   width: 100%;
-  border: 1px solid #D2D9DA;
-  border-radius: 1rem;
   background-color: #fff;
-  margin-top: 1.6rem;
+  border: 0.1rem solid #d2d9da;
+  border-radius: 1rem;
+`;
+
+export const InfoDetails = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 2.8rem;
   display: flex;
-  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
   gap: 5.6rem;
 
   .itens-wrapper {
@@ -41,108 +58,453 @@ export const Display = styled.div`
     margin: 2.8rem 0 2.8rem 2.8rem;
 
     span {
-      font-family: 'Noto Sans', sans-serif;
+      font-family: "Noto Sans", sans-serif;
       font-size: 1.4rem;
       font-weight: 600;
       font-style: normal;
-      line-height: 2.0rem;
-      color: #00808A;
+      line-height: 2rem;
+      color: #00808a;
     }
 
     p {
-      font-family: 'Noto Sans', sans-serif;
+      font-family: "Noto Sans", sans-serif;
       font-size: 1.6rem;
       font-weight: 600;
       font-style: normal;
-      line-height: 2.0rem;
+      line-height: 2rem;
       color: #354344;
     }
   }
 `;
 
-export const BoxWrapper = styled.div`
+export const GrafficDataWrapper = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: row;
   margin-top: 2.4rem;
+  margin-bottom: 2.4rem;
   gap: 2.4rem;
 `;
 
-export const GraphicDisplay = styled.div`
-  display: flex;
-  flex-direction: column;
-
+export const IntegrationsWrapper = styled.div`
   width: 100%;
   height: 44.4rem;
-  background-color: #fff;
-  border: 1px solid #D2D9DA;
-  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
 
-  .text-and-icon {
+  .header {
+    width: 100%;
     display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 1.5rem;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    gap: 0.8rem;
+    padding-top: 1.6rem;
+    padding-left: 1.9rem;
 
-    margin: 2.2rem 39.0rem 0 2.0rem; 
+    .title {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      gap: 0.8rem;
 
-    > h1 {
-      font-family: 'Noto Sans', sans-serif;
-      font-size: 1.6rem;
-      font-weight: 700;
+      h1 {
+        font-family: "Noto Sans", sans-serif;
+        font-size: 1.6rem;
+        font-weight: 700;
+        font-style: normal;
+        line-height: normal;
+        color: #354344;
+      }
+    }
+
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
       font-style: normal;
+      font-weight: 400;
       line-height: normal;
       color: #354344;
     }
   }
 
-  > p {
-    font-family: 'Noto Sans', sans-serif;
-    font-size: 1.4rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-    color: #354344;
-
-    margin: 1rem 46.6rem 0 2.0rem;
+  .content {
+    width: 100%;
+    height: 100%;
+    margin-top: 2.4rem;
   }
 `;
 
-export const TimeGraphic = styled.div`
-  width: 100%;
-  height: 30rem;
+export const EdTechNameTableCollumn = styled.div`
+  width: 31rem;
 
-  margin-top: 2.4rem;
-  
-  border: 1px solid #D2D9DA;
-  border-radius: 1rem;
-  background-color: #fff;
-
-  .text-and-icon {
+  .table-header {
+    padding-left: 1.6rem;
     display: flex;
-    flex-direction: row;
     align-items: center;
-    gap: 1.5rem;
+    justify-content: flex-start;
 
-    margin: 2.2rem 39.0rem 0 2.0rem; 
-
-    > h1 {
-      font-family: 'Noto Sans', sans-serif;
-      font-size: 1.6rem;
-      font-weight: 700;
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
       font-style: normal;
-      line-height: normal;
+      font-weight: 700;
+      color: #9da8a9;
+    }
+  }
+
+  .table-row {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    padding-left: 1.6rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 400;
       color: #354344;
     }
   }
 `;
 
-export const BoxTable = styled.div`
-  width: 100%;
-  height: 15.6rem;
-  
-  border: 1px solid #D2D9DA;
-  border-radius: 1rem;
-  background-color: #fff;
-  margin-top: 2.4rem;
+export const EdTechStatusTableCollumn = styled.div`
+  width: 29rem;
+
+  .table-header {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 700;
+      text-decoration: none;
+      color: #9da8a9;
+    }
+  }
+
+  .table-row {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 400;
+      color: #354344;
+    }
+
+    .green-dot {
+      width: 0.8rem;
+      height: 0.8rem;
+      border-radius: 100rem;
+      background-color: #24a31a;
+      outline: 0.3rem solid #acd49f;
+      margin-right: 1.1rem;
+    }
+  }
 `;
 
+export const TimestampMetric = styled.div`
+  width: 100%;
+  height: 30.7rem;
+
+  .header {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    gap: 0.8rem;
+    padding-top: 1.6rem;
+    padding-left: 1.9rem;
+
+    .title {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      gap: 0.8rem;
+
+      h1 {
+        font-family: "Noto Sans", sans-serif;
+        font-size: 1.6rem;
+        font-weight: 700;
+        font-style: normal;
+        line-height: normal;
+        color: #354344;
+      }
+    }
+  }
+
+  .content {
+    width: 100%;
+    height: 100%;
+  }
+`;
+export const EdTechDatalog = styled.div`
+  width: 100%;
+  padding-top: 1.3rem;
+`;
+
+export const EdTechDatalogStatusTableCollumn = styled.div`
+  width: 10.4rem;
+
+  .table-header {
+    padding-left: 2.2rem;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 700;
+      color: #9da8a9;
+    }
+  }
+
+  .table-row {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    padding-left: 2.2rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 400;
+      color: #354344;
+    }
+
+    .status-ok{
+      width: 3.5rem;
+      height: 3.5rem;
+      border-radius: 1rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #EEF6EB;
+      border: 0.1rem solid #ACD49F;
+    }
+  }
+`;
+
+export const EdTechDatalogDescriptionTableCollumn = styled.div`
+  width: 19.2rem;
+
+  .table-header {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 700;
+      color: #9da8a9;
+    }
+  }
+
+  .table-row {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 400;
+      color: #354344;
+    }
+  }
+`;
+
+export const EdTechDatalogDateStartTableCollumn = styled.div`
+  width: 16.5rem;
+
+  .table-header {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 700;
+      color: #9da8a9;
+    }
+  }
+
+  .table-row {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 400;
+      color: #354344;
+    }
+  }
+`;
+
+export const EdTechDatalogDateEndTableCollumn = styled.div`
+  width: 24.6rem;
+
+  .table-header {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 700;
+      color: #9da8a9;
+    }
+  }
+
+  .table-row {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 400;
+      color: #354344;
+    }
+  }
+`;
+
+export const EdTechDatalogLatencyTableCollumn = styled.div`
+  width: 17.8rem;
+
+  .table-header {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 700;
+      color: #9da8a9;
+    }
+  }
+
+  .table-row {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 400;
+      color: #354344;
+    }
+  }
+`;
+
+export const EdTechDatalogProcessTableCollumn = styled.div`
+  width: 20.6rem;
+
+  .table-header {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 700;
+      color: #9da8a9;
+    }
+  }
+
+  .table-row {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 400;
+      color: #354344;
+    }
+  }
+`;
+
+export const EdTechDatalogErrorTableCollumn = styled.div`
+  width: 13.2rem;
+
+  .table-header {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 700;
+      color: #9da8a9;
+    }
+  }
+
+  .table-row {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    span {
+      font-family: "Noto Sans", sans-serif;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 400;
+      color: #354344;
+    }
+  }
+`;
